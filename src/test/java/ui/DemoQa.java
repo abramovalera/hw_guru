c6c2c880304f28fb3f73ce7951959ca2ce5b72fa
v <abramovaera15@mail.ru>
2026-01-09 22:04:24 +0300

    hw 5

--- src/test/java/ui/DemoQa.java
+++ src/test/java/ui/DemoQa.java
@@ -3,7 +3,6 @@ package ui;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import utils.Clean;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
